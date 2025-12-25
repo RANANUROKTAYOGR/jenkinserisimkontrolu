f444c987134860333db5867765d4cc297191b321
RANANUROKTAY <136755355+RANANUROKTAY@users.noreply.github.com>
2025-12-25 14:13:54 +0300

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Worlddddd!");
+        System.out.println("Hello, Worldddddddd!");
+        System.out.println("Hello, Worldddddddd!");
     }
 }
